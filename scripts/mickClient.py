e4dee199a47734bbe997e37aee74a5ae2496ff78
michael <mickwald@hotmail.com>
2017-02-16 11:32:38 +0100

    Added basic functionality

--- scripts/mickClient.py
+++ scripts/mickClient.py
@@ -1,7 +1,32 @@
 import rospy
 from chat_server.msg import Message
 
-message = raw_input('Enter message: ')
-print("Your message is \"" + message + "\"")
 
-def run_client():
+def select_mode():
+    mode = 0
+    while mode != 'read' || mode != 'send':
+        mode = raw_input('Enter mode (read/send):' + mode)
+    if mode == 'read':
+        read_client()
+    if mode == 'send':
+        send_client()
+    return
+
+def send_client():
+    pub = rospy.Publisher('chat_out', Message, queue_size=10)
+    rospy.init_node('mick_sender', anonymous=True)
+    name = 'mickwald'
+    message = 0
+    while message != '/exit':
+        message = raw_input('Message: ')
+        //pub.publish(name,message)
+    return
+
+def read_client():
+
+
+if __name__ == "__main__":
+    try:
+        select_mode()
+    except rospy.ROSInterruptException:
+        pass
